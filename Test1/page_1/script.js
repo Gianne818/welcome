d6c2684af3ac14d6d6068e1cd80466f41735bca3
Gianne818 <cruzgian008@gmail.com>
2025-05-12 17:03:38 +0800

    Add files via upload

---
+++ Test1/page_1/script.js
@@ -0,0 +1,66 @@
+let errorCount = 0;
+let sound = document.getElementById('music_1');
+let button = document.getElementById('soundOn')
+sound.play();
+
+button.addEventListener("click", event=>{
+    if(sound.paused){
+        sound.play();
+        button.src = "Embeds/sound_on.png";
+    }
+    else if(sound.play()){
+        sound.pause();
+        button.src = "Embeds/sound_off.png";
+    }
+});
+
+
+
+function formValidation(){
+    event.preventDefault();
+    let  username = document.getElementById('username');
+    let pass = document.getElementById('password');
+    let myForm = document.getElementById('userForm');
+    let userError = document.getElementById('userError');
+    let passError = document.getElementById('passError');
+    let hint = document.getElementById('hint');
+    
+    userError.style.opacity = '0';
+    passError.style.opacity = '0';
+    hint.style.opacity = '0';
+    userError.textContent = "";
+    passError.textContent = "";
+    hint.textContent = "";
+    
+
+    let isError = false;
+    
+
+    if(username.value !== "09321496633"){
+        userError.textContent = "Incorrect Username!";
+        userError.style.opacity = '1';
+        isError = true;
+        errorCount++;
+    }
+
+    if(pass.value !== "crssmy"){
+        passError.textContent = "Incorrect Password!";
+        passError.style.opacity = '1';
+        isError = true;
+        errorCount++;
+    }
+
+    if(isError){
+        if(errorCount>=5){
+            hint.style.opacity = '1';
+            hint.textContent = "Hint: Its the username and password you always use!";
+        }
+        return false;
+    } 
+    if (!isError){
+        console.log("Validation successful, redirecting...");
+        window.location.href = '../page_2/index.html';
+        return true;
+    }
+   
+}
